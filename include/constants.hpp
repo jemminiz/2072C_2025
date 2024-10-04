ae288e902ca5222c6908ec2d4dc2d0cc3be8c1d3
Jonathan <jonathan.emminizer@yahoo.com>
2024-10-03 21:43:39 -0400

    Made some stuff do some things

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,8 +1,36 @@
 #ifndef _CONSTANTS_HPP
 #define _CONSTANTS_HPP
+#include <cstdint>
 
-#pragma region Ports
+namespace StratusQuo
+{
+    #pragma region Smart_Ports
+    inline constexpr int LEFT_MOTOR_BACK_PORT (5);
+    inline constexpr int LEFT_MOTOR_MID_PORT (-3);
+    inline constexpr int LEFT_MOTOR_FRONT_PORT (-8);
+    inline constexpr int RIGHT_MOTOR_BACK_PORT (-1);
+    inline constexpr int RIGHT_MOTOR_MID_PORT (2);
+    inline constexpr int RIGHT_MOTOR_FRONT_PORT (11);
+    inline constexpr int ARM_PORT (10);
+    inline constexpr int INTAKE_PORT (7);
+    inline constexpr uint8_t IMU_PORT (12);
+    #pragma endregion Smart_Ports
+    
+    #pragma region Piston_Ports
+    inline constexpr uint8_t CLAMP_PORT ('g');
+    inline constexpr uint8_t INTAKE_PISTON_PORT ('b');
+    inline constexpr uint8_t LIMIT_SWITCH_PORT ('h');
+    inline constexpr uint8_t SCOOP_PORT ('f');
+    inline constexpr uint8_t BASKET_PORT ('a');
+    #pragma endregion Piston_Ports
 
-#pragma endregion Ports
+    #pragma region Const_Vals
+    inline constexpr bool BASKET_START_EXTENDED (false);
+    inline constexpr bool INTAKE_STARTING_PISTON_STATE (true);
+    inline constexpr bool SCOOP_START_EXTENDED (false);
+    inline constexpr float WHEEL_DIAMETER (3.25);
+    inline constexpr int RPM (450);
+    #pragma endregion Const_Vals
+}
 
 #endif
--- src/autons.cpp
+++ src/autons.cpp
@@ -6,7 +6,7 @@
 /////
 
 // These are out of 127
-const int DRIVE_SPEED = 110;
+const int DRIVE_SPEED = 127;
 const int TURN_SPEED = 90;
 const int SWING_SPEED = 90;
 
