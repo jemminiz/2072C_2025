0de1161a75fa1e4d89dabc9b4c98cab622cf15e9
Jonathan Emminizer <72472839+jemminiz@users.noreply.github.com>
2024-10-03 21:14:07 -0400

    Delete src directory

--- src/constants.cpp
+++
@@ -1,4 +0,0 @@
-#include "constants.h"
-
-std::vector<int8_t> StratusQuo::left_motors{StratusQuo::LEFT_MOTOR_BACK_PORT, StratusQuo::LEFT_MOTOR_MID_PORT, StratusQuo::LEFT_MOTOR_FRONT_PORT};
-std::vector<int8_t> StratusQuo::right_motors{StratusQuo::RIGHT_MOTOR_BACK_PORT, StratusQuo::RIGHT_MOTOR_MID_PORT, StratusQuo::RIGHT_MOTOR_FRONT_PORT};
--- src/scooper.cpp
+++
@@ -1,9 +0,0 @@
-#include "scooper.h"
-
-StratusQuo::Scooper::Scooper(int8_t port, bool start_extended) : scooper(port, start_extended)
-{};
-
-void StratusQuo::Scooper::toggle()
-{
-    scooper.toggle();
-}
