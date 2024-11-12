8f1e0cd9f5a25b2c3def5859676e1434c2b63c05
Jonathan Emminizer <jonathan.emminizer@yahoo.com>
2024-11-12 07:32:05 -0500

    Auton work at Karma's house - somewhat consistent AWP

--- src/autons.cpp
+++ src/autons.cpp
@@ -1,4 +1,5 @@
 #include "autons.hpp"
+#include <cmath>
 #include "lemlib/chassis/chassis.hpp"
 #include "subsystems.hpp" // IWYU pragma: keep
 
@@ -21,25 +22,24 @@ void StratusQuo::sig_blue_goal_side()
 {
   using namespace StratusQuo;
   robot.chassis.setPose(5, 12, 180);
-  robot.chassis.moveToPoint(5, 28, 1100, {.forwards = false});
+  robot.chassis.moveToPoint(5, 25, 1100, {.forwards = false});
   robot.chassis.waitUntilDone();
+  double dist = std::sqrt((54.58 - 25) * (54.58 - 25) + (17.1 - 5) * (17.1 - 5));
   pros::delay(100);
-  robot.chassis.moveToPoint(13.75, 50.5, 2500, {.forwards = false, .maxSpeed = 60});
-  robot.chassis.waitUntilDone();
-  pros::delay(500);
-  /*
+  robot.chassis.moveToPoint(17.1, 54.58, 2500, {.forwards = false, .maxSpeed = 60});
+  robot.chassis.waitUntil(dist);
   robot.clamp.extend();
   robot.intake.move(-127);
   pros::delay(400);
   robot.intake.move(127);
   robot.chassis.waitUntilDone();
-  robot.chassis.moveToPoint(17, 45, 1000);
+  robot.chassis.moveToPoint(22, 48, 1000);
   pros::delay(2000);
   robot.chassis.waitUntilDone();
   robot.clamp.retract();
   robot.intake.brake();
   robot.chassis.turnToHeading(270, 1000);
-  robot.chassis.moveToPoint(45, 37, 1000, {.forwards = false, .maxSpeed = 60});
+  robot.chassis.moveToPoint(45, 44, 1000, {.forwards = false, .maxSpeed = 60});
   robot.chassis.waitUntilDone();
   robot.intake.move(-127);
   robot.clamp.extend();
@@ -49,8 +49,7 @@ void StratusQuo::sig_blue_goal_side()
   robot.chassis.moveToPoint(70, 27, 2500);
   robot.chassis.waitUntilDone();
   robot.intake.toggle();
-*/
-
+  robot.chassis.moveToPoint(63, 24.3, 1000, {.forwards = false});
   /*
   robot.chassis.moveToPoint(46, 45, 1000, {.forwards = false, .maxSpeed = 90});
   robot.clamp.retract();
