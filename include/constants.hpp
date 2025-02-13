7e2686b7e84769436ac6062aa81aa7953c90c372
Jonathan <jonathan.emminizer@yahoo.com>
2025-02-13 09:41:50 -0500

    Added gear ratio to get_position for lady brown, fixed styling on constants.hpp

--- include/constants.hpp
+++ include/constants.hpp
@@ -7,11 +7,13 @@
 inline const int ROLLER_PORT = 9;
 inline const int HOOK_PORT = 8;
 inline const int LADY_BROWN_PORT = 10;
+
 inline const uint8_t INTAKE_PISTON_PORT = 'b';
 inline const uint8_t CLAMP_PORT = 'a';
 inline const uint8_t LEFT_LIMIT_SWITCH_PORT = 'c';
 inline const uint8_t RIGHT_LIMIT_SWITCH_PORT = 'd';
-inline const ez::PID LADY_BROWN_PID(0.5, 0, 10.0);
+
+inline const ez::PID LADY_BROWN_PID(0.65, 0, 0.0);
 inline const pros::Rotation LADY_BROWN_ROTATION(-21);
 
 inline std::atomic<bool> is_auto_clamp_enabled(true);
