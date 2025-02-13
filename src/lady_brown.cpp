b662762504f6d38a0e8d19cb1195d854179a8bdb
Jonathan <jonathan.emminizer@yahoo.com>
2025-02-13 13:06:50 -0500

    Added brake mode to constructor for lady brown instead of set_constants

--- src/lady_brown.cpp
+++ src/lady_brown.cpp
@@ -3,7 +3,9 @@
 #include "pros/motors.h"
 
 StratusQuo::Lady_Brown::Lady_Brown(const int port, const ez::PID pid, const pros::Rotation rotation) : _motor(port), _pid(pid), _rotation(rotation)
-{};
+{
+    _motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+};
 
 int StratusQuo::Lady_Brown::move(int8_t voltage)
 {
@@ -53,7 +55,6 @@ void StratusQuo::Lady_Brown::pid_wait()
 void StratusQuo::Lady_Brown::set_exit_conditions(double small_error_time, double small_error, double big_error_time, double big_error, int velocity_exit_time, int mA_timeout)
 {
     _pid.exit_condition_set(small_error_time, small_error, big_error_time, big_error, velocity_exit_time, mA_timeout);
-    _motor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 }
 
 void StratusQuo::Lady_Brown::target_set(int target)
