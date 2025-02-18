d5600a5ca8ca917e1886c5d28a969e99dfd6d8ec
Jonathan <jonathan.emminizer@yahoo.com>
2025-02-18 16:26:12 -0500

    Limit switch ports

--- include/constants.hpp
+++ include/constants.hpp
@@ -8,10 +8,10 @@ inline const int ROLLER_PORT = 9;
 inline const int HOOK_PORT = 8;
 inline const int LADY_BROWN_PORT = 10;
 
-inline const uint8_t INTAKE_PISTON_PORT = 'b';
+inline const uint8_t INTAKE_PISTON_PORT = 'd';
 inline const uint8_t CLAMP_PORT = 'a';
 inline const uint8_t LEFT_LIMIT_SWITCH_PORT = 'c';
-inline const uint8_t RIGHT_LIMIT_SWITCH_PORT = 'd';
+inline const uint8_t RIGHT_LIMIT_SWITCH_PORT = 'b';
 
 inline const pros::Rotation LADY_BROWN_ROTATION(-21);
 
